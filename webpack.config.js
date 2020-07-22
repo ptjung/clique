7bf324956b0513126d4b718c37ab1fff6ea7ba26
Patrick <patrickjung@rogers.com>
2020-07-21 23:35:22 -0400

    Commit for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-    entry: __dirname + "/src/index.js",
+    entry: ['./src/index.js'],
     output: {
-        path: __dirname + "/public",
+        path: __dirname,
         publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: __dirname + "/public/",
-        inline: true,
-        host: '0.0.0.0',
-        port: 8080
+        filename: 'bundle.js',
+        chunkFilename: '[name].js'
     }
 }
