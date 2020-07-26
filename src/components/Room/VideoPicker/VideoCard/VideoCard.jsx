6e91c08ff338f43d42300a375fdb80019d9dffc7
Patrick <patrickjung@rogers.com>
2020-07-26 15:54:24 -0400

    Properly decodes HTML

--- src/components/Room/VideoPicker/VideoCard/VideoCard.jsx
+++ src/components/Room/VideoPicker/VideoCard/VideoCard.jsx
@@ -4,17 +4,23 @@ import styles from './VideoCard.module.css';
 import cx from 'classnames';
 
 function VideoCard(props) {
+    const converterHTML = document.createElement("textarea");
+
+    const extractHTML = (paramText) => {
+        converterHTML.innerHTML = paramText;
+        return converterHTML.value;
+    }
 
     return (
         <Card className={cx(styles.videoCard, styles.unselectable)} onClick={() => props.clickHandler(props.videoId)} title={props.videoName}>
             <Card.Img className={styles.unselectable} variant="top" src={props.videoThumb} />
             <Card.Body className={styles.videoTitleWrapper}>
                 <Card.Text className={styles.videoTitleText}>
-                    {props.videoName}
+                    {extractHTML(props.videoName)}
                 </Card.Text>
             </Card.Body>
             <Card.Text className={styles.videoAuthorText}>
-                {props.channelName}
+                {extractHTML(props.channelName)}
             </Card.Text>
         </Card>
     )
